3f6445d95f3f82e409102389991edc437993adfe
PriscilaVeysaga <pri_veysaga@hotmail.com>
2022-04-04 10:57:51 -0300

    first commit

---
+++ projeto_01/projeto_01/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim nota1, nota2, media As Double
+        Dim aluno As String
+
+        Console.WriteLine("Digite o nome do aluno: ")
+        aluno = Console.ReadLine()
+
+        Console.WriteLine("Digite a primeira nota: ")
+        nota1 = Console.ReadLine()
+
+        Console.WriteLine("Digite a segunda nota: ")
+        nota2 = Console.ReadLine()
+
+        media = (nota1 + nota2) / 2
+
+        Console.WriteLine("A media do aluno " & aluno & " é igual a " & media)
+
+
+    End Sub
+
+End Module
